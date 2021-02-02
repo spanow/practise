64f746070bc6a9f9acb1123366429a9a539fa4a2
spanow <manil.masdoua@dauphine.eu>
2021-02-02 16:22:50 +0100

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 
@@ -15,7 +14,6 @@ import { ProductListComponent } from "./product-list/product-list.component";
   ],
   declarations: [
     AppComponent,
-    HelloComponent,
     TopBarComponent,
     ProductListComponent
   ],
