acd157c7f7762eaf00cf81a16252758006b1b23e
spanow <manil.masdoua@dauphine.eu>
2021-02-02 16:07:13 +0100

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot([])],
-  declarations: [AppComponent, HelloComponent, TopBarComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: "", component: ProductListComponent }])
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
